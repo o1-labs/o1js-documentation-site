46a0e94a80c2dd62d99db05bfdf1b83d21f717dc
Coby <coby@o1labs.org>
2025-03-17 18:06:24 -0400

    switch to remark-code-snippets

---
+++ docs/snippets/field/constants.ts
@@ -0,0 +1,3 @@
+import { Field } from "o1js";
+
+export const max_size = BigInt(Field.ORDER.toString()).toLocaleString();
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          remarkPlugins: [require("remark-code-import")],
+          remarkPlugins: [require("remark-code-snippets")],
         },
         theme: {
           customCss: "./src/css/custom.css",
