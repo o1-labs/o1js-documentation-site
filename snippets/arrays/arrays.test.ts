5e9ff99ec5d674d4ac407699e0fef91a04abd2f7
Coby <coby@o1labs.org>
2025-04-01 17:22:50 -0400

    add more basic types documentation

--- snippets/arrays/arrays.test.ts
+++ snippets/arrays/arrays.test.ts
@@ -5,4 +5,5 @@ import { Field } from "o1js";
 
 it("Witnessed field array is equivalent to a JS array of Fields", () => {
   expect(fieldArray).toMatchObject([Field(1), Field(2), Field(3)]);
+  expect(fieldArray.map((x) => x.toBigInt())).toMatchObject([1n, 2n, 3n]);
 });
