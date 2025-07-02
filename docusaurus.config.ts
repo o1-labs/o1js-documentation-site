f31dfd6017b8d2229236c24b7b9b5fed2a9c1079
Coby <coby@o1labs.org>
2025-07-02 16:29:55 -0400

    add vercel json

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: "https://documentation.o1labs.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/o1js",
+  baseUrl: "/o1js/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
