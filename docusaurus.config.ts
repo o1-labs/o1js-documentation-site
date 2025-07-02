d8dd5f45afe0eff44a86a220cc08fcef01242160
ymekuria <ymekuria@gmail.com>
2025-07-02 02:01:27 +0200

    fix(docusaurus.config.ts): update baseUrl to root path to fix broken links and ensure correct deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: "https://documentation.o1labs.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/documentation-site",
+  baseUrl: "/",
 
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
