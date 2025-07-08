7f5ae3f0cfafe991718670c9fe924da2dd6d111e
Coby <coby@o1labs.org>
2025-07-08 13:20:20 -0400

    tweak

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // Set the production url of your site here
   url: "https://documentation.o1labs.org",
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: "/o1js/",
+  baseUrl: "/",
 
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
